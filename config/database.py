da996c0909b04c11f32f461c443e850a5aed7658
Snickmax <victor.2002.espinoza@gmail.com>
2024-12-13 18:42:40 -0300

    Update database for scanner

--- config/database.py
+++ config/database.py
@@ -10,9 +10,8 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
-# DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
-DATABASE_URL = f"sqlite:///tareas.db"
+DATABASE_URL = "sqlite:///tareas.db"
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
